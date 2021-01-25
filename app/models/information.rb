f18b9220e6d1af9d58dff04b3e9d8540d51a9215
heliolite <ameji-yukari@ozzio.jp>
2021-01-25 06:43:24 +0000

    身長入力140以上200以下

--- app/models/information.rb
+++ app/models/information.rb
@@ -1,6 +1,7 @@
 class Information < ApplicationRecord
   belongs_to :character
 
+  #身長は140cm以上200cm以下
   validates :height, numericality: { only_integer: true,
                                      greater_than_or_equal_to: 140,
                                      less_than_or_equal_to: 200 }
