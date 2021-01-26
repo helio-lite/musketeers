522f50311bb7d94d41e3ff3a58cf127d0234862f
heliolite <ameji-yukari@ozzio.jp>
2021-01-26 06:14:24 +0000

    information件数にバリデーション

--- app/models/information.rb
+++ app/models/information.rb
@@ -1,15 +1,25 @@
 class Information < ApplicationRecord
   belongs_to :character
 
+  before_validation :info_sprit #入力値前後のスペース除去
   #身長は140cm以上200cm以下
   validates :height, numericality: { only_integer: true,
                                      greater_than_or_equal_to: 140,
                                      less_than_or_equal_to: 200 }
-  before_validation :info_sprit #入力値前後のスペース除去
+  validate  :info_count #銃一種につきinformationは2件まで
+
+  private
+
+  def info_count
+    if character.information.count >= 2
+      errors.add(:character, "銃一種につきinformationは2件です")
+    end
+  end
 
   def info_sprit
     self.introduction.strip!
     self.hobby.strip!
     self.favorite.strip!
   end
+
 end
