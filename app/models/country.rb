7413ed4605ef989699e77099ac534904211663ac
heliolite <ameji-yukari@ozzio.jp>
2021-02-16 06:29:06 +0000

    国をルビ順に並び替え

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,13 +1,15 @@
 class Country < ActiveHash::Base
   self.data = [
-    {id: 1, name: "アメリカ"},
-    {id: 2, name: "イングランド"},
-    {id: 3, name: "オーストリア"},
-    {id: 4, name: "ドイツ"},
-    {id: 5, name: "トルコ"},
-    {id: 6, name: "日本"},
-    {id: 7, name: "フランス"},
-    {id: 8, name: "ベルギー"},
-    {id: 9, name: "ロシア"}
+    {id: 0, name: "???", ruby: "???"},
+    {id: 1, name: "アメリカ", ruby: "アメリカ"},
+    {id: 2, name: "イングランド", ruby: "イングランド"},
+    {id: 3, name: "オーストリア", ruby: "オーストリア"},
+    {id: 4, name: "ドイツ", ruby: "ドイツ"},
+    {id: 5, name: "トルコ", ruby: "トルコ"},
+    {id: 6, name: "日本", ruby: "ニホン"},
+    {id: 7, name: "フランス", ruby: "フランス"},
+    {id: 8, name: "ベルギー", ruby: "ベルギー"},
+    {id: 9, name: "ロシア", ruby: "ロシア"},
+    {id: 10, name: "フィンランド", ruby: "フィンランド"}
   ]
 end
