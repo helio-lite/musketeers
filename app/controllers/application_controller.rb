6232692b8ef7ebe45f1d237b5484c4d62f8fc877
heliolite <ameji-yukari@ozzio.jp>
2021-03-16 04:31:09 +0000

    検索条件引き継ぎ追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+
+  def take_params
+
+    @gun_category = params[:gun_category].present? ? {gun_category_id: params[:gun_category][:gun_category_id]} : nil
+    {
+     :keyword => params[:keyword],
+     :gun_category => @gun_category,
+     :gun_type => params[:gun_type],
+     :country => params[:country],
+     :page     => params[:page],
+     :per_page => params[:per_page]
+    }
+  end 
+  helper_method :take_params
+
 end
